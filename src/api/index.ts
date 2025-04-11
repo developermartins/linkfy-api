f968baa7514b04ee3af4e6e70a9e4a8fe2e48ecd
developermartins <martssilvapro@gmail.com>
2025-04-11 18:34:32 -0300

    organize project

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,13 @@
-import express from 'express';
+// import express from 'express';
 
-import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
+// import MessageResponse from '../interfaces/MessageResponse';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏',
-  });
-});
+// router.get<{}, MessageResponse>('/', (req, res) => {
+//   res.json({
+//     message: 'API - 👋🌎🌍🌏',
+//   });
+// });
 
-router.use('/emojis', emojis);
-
-export default router;
+// export default router;
--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 
-import * as middlewares from './middlewares';
-import api from './api';
-import MessageResponse from './interfaces/MessageResponse';
+import * as middlewares from './middlewares/middlewares';
 
 require('dotenv').config();
 
@@ -16,13 +14,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
-
-app.use('/api/v1', api);
+app.use('/api/v1',);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
